c35f894332475db576929a364ab603b347bceccd
Noble Benjamin <nobleben2008@gmail.com>
2024-07-03 22:41:21 +0100

    Mapped the faq state

--- src/components/ItemCon.jsx
+++ src/components/ItemCon.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import Item from './Item'
 
-const ItemCon = () => {
+const ItemCon = ({faq, setFaqs}) => {
   return (
     <div className="accordion">
-      
+      {faq.map(item => <Item faq={item}/>)}
     </div>
   )
 }
