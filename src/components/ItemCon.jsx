7ac3fdce02a75a2ed8ee8f7a972e9992d704fd08
Noble Benjamin <nobleben2008@gmail.com>
2024-07-04 21:21:22 +0100

    Passed the curOpen and setOpen state to the Itm Comp

--- src/components/ItemCon.jsx
+++ src/components/ItemCon.jsx
@@ -5,7 +5,7 @@ const ItemCon = ({faq, setFaqs}) => {
   const [curOpen, setOpen] = useState(null)
   return (
     <div className="accordion">
-      {faq.map((item, i) => <Item faq={item} setFaqs={setFaqs} i={i}/>)}
+      {faq.map((item, i) => <Item faq={item} setFaqs={setFaqs} i={i} curOpen={curOpen} onOpen={setOpen}/>)}
     </div>
   )
 }
