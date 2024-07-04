2b46bf40a3c0314a3afc7bf7742c8d8631eab568
Noble Benjamin <nobleben2008@gmail.com>
2024-07-04 21:26:57 +0100

    Used the onOpen prop in the click of an Item

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const Item = (props) => {
   return (
     <div className={`item ${isOpen ? 'open': ''}`} onClick={() => {
-        setOpen(current => !current)
+        props.onOpen(props.i)
     }}>
     <p className='number'>{props.i <= 9 ? `0${props.i}`: `${props.i}`}</p>
     <p className='title'>{props.faq.title}</p>
