5f69c6475c54424c13fa02ab92f3dc2dc1791652
Noble Benjamin <nobleben2008@gmail.com>
2024-07-04 21:18:10 +0100

    Defined a curOpen state to monitor open Items

--- src/components/ItemCon.jsx
+++ src/components/ItemCon.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Item from './Item'
 
 const ItemCon = ({faq, setFaqs}) => {
+  const [curOpen, setOpen] = useState(null)
   return (
     <div className="accordion">
       {faq.map((item, i) => <Item faq={item} setFaqs={setFaqs} i={i}/>)}
