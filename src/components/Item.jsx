d21278c47bec8923f5fafa674d89ddb06a75b1e5
Noble Benjamin <nobleben2008@gmail.com>
2024-07-04 21:24:45 +0100

    Used the curOpen State in the Item comp

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 
 const Item = (props) => {
-    const [isOpen, setOpen] = useState(false)
   return (
     <div className={`item ${isOpen ? 'open': ''}`} onClick={() => {
         setOpen(current => !current)
@@ -10,7 +9,7 @@ const Item = (props) => {
     <p className='title'>{props.faq.title}</p>
     <p className='icon'>{isOpen ? '-' : '+'}</p>
 
-    {isOpen && <div className='content-box'>{props.faq.text}</div>}
+    {props.curOpen == props.i && <div className='content-box'>{props.faq.text}</div>}
 
     </div>
   )
