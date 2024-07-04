b96e0f9ee945f0044ddb8ba7aee3f53f37e07172
Noble Benjamin <nobleben2008@gmail.com>
2024-07-04 21:37:09 +0100

    Made an open  item close when it is clicked

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 const Item = (props) => {
   return (
     <div className={`item ${props.curOpen == props.i ? 'open': ''}`} onClick={() => {
-        props.onOpen(props.i)
+        props.onOpen(props.curOpen == props.i ? null : props.i);
     }}>
     <p className='number'>{props.i <= 9 ? `0${props.i}`: `${props.i}`}</p>
     <p className='title'>{props.faq.title}</p>
