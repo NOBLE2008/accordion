134c2bbdb98efe0f9276e80bda72e74d5bb65c58
Noble Benjamin <nobleben2008@gmail.com>
2024-07-04 21:29:23 +0100

    Removed parts where isOpen was called in the Iten Comp

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,12 +2,11 @@ import React, { useState } from 'react'
 
 const Item = (props) => {
   return (
-    <div className={`item ${isOpen ? 'open': ''}`} onClick={() => {
+    <div className={`item ${props.curOpen == props.i ? 'open': ''}`} onClick={() => {
         props.onOpen(props.i)
     }}>
     <p className='number'>{props.i <= 9 ? `0${props.i}`: `${props.i}`}</p>
     <p className='title'>{props.faq.title}</p>
-    <p className='icon'>{isOpen ? '-' : '+'}</p>
 
     {props.curOpen == props.i && <div className='content-box'>{props.faq.text}</div>}
 
